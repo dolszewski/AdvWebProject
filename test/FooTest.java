69aa0f5882775e65b15e96b05ad78c72b9441676
Dan Olszewski <dolszewski@carroll.edu>
2018-01-19 13:28:30 -0700

    added test route for foo route

---
+++ test/FooTest.java
@@ -0,0 +1,20 @@
+
+import org.junit.Test;
+import play.mvc.Result;
+import play.test.Helpers;
+import play.test.WithApplication;
+
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
+
+
+public class FooTest extends WithApplication{
+
+	@Test
+	public void testFooRout() {
+		Result result = route(fakeRequest(GET, "/foo"));
+		assertThat(result).isNotNull();
+	}
+	
+	
+}
