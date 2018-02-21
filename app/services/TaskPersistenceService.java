9391126d2a710ee060c362ad47e9cc755b760f4e
Dan Olszewski <dolszewski@carroll.edu>
2018-02-21 16:37:20 -0700

    Added implementation of TaskPeristence

---
+++ app/services/TaskPersistenceService.java
@@ -0,0 +1,13 @@
+package services;
+
+import models.Task;
+
+import java.util.List;
+
+public interface TaskPersistenceService {
+
+    void saveTask(Task t);
+
+    List<Task> fetchAllTasks();
+
+}
---
+++ app/services/TaskPersistenceServiceImpl.java
@@ -0,0 +1,21 @@
+package services;
+
+import models.Task;
+
+import play.db.jpa.JPA;
+
+import java.util.List;
+
+public class TaskPersistenceServiceImpl implements TaskPersistenceService {
+
+  @Override
+  public void saveTask(Task task) {
+      JPA.em().persist(task);
+  }
+
+  @Override
+  public List<Task> fetchAllTasks() {
+    return JPA.em().createQuery("from Task", Task.class).getResultList();
+  }
+
+}
