371bbcd82b8b22675a7d68222b58c34c2ab41ca3
Dan Olszewski <dolszewski@carroll.edu>
2018-02-21 16:39:29 -0700

    Added task persistence

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,28 +2,60 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
+import java.util.List;
 import views.html.*;
+import models.Task;
+import play.db.jpa.JPA;
+
+import services.TaskPersistenceService;
+import services.TaskPersistenceServiceImpl;
+
+import views.html.index;
+
+
+
 
 public class Application extends Controller {
+
+	private static final TaskPersistenceService taskPersist = new TaskPersistenceServiceImpl();
 
     public static Result index() {
         return ok(index.render("Hello World!", play.data.Form.form(models.Task.class)));
     }
     
+    @play.db.jpa.Transactional
     public static Result addTask() {
     		play.data.Form<models.Task> form = play.data.Form.form(models.Task.class).bindFromRequest();
     		if(form.hasErrors()) {
     			return badRequest(index.render("Hello World!", form));
     		}
-    		else {
-	    		models.Task task = form.get();
-	    		task.save();
-	    		return redirect(routes.Application.index());
-    		}
+    		
+	    	models.Task task = form.get();
+	    taskPerist.saveTask(task);
+
+	    	return redirect(routes.Application.index());
+    		
     }
+    @play.db.jpa.Transactional
     public static Result getTasks() {
-    		java.util.List<models.Task> tasks = new play.db.ebean.Model.Finder(String.class, models.Task.class).all();
+        List<Task> tasks = JPA.em().createQuery("from Task", Task.class).getResultList();
     		return ok(play.libs.Json.toJson(tasks));
     }
+    
+ /*
+  *  @play.db.jpa.Transactional
+    public static Result getTasks(Integer searchId) {
+    		java.util.List<models.Task> tasks = play.db.jpa.JPA.em()
+    				.createQuery("FROM Task t WHERE t.id = :id", models.Task.class)
+    				.setParameter("id",searchId )
+    				.getResultList();
+   /* 		if(tasks == null || tasks.isEmpty()) {
+    			return null;
+    		} else {
+    			return tasks.get(0);
+    		}
+    		return ok(play.libs.Json.toJson(tasks));
+    }  
+  */
 }
+
