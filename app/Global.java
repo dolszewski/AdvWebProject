f3b6396966ee60c8011cccb1179a2c0a7a05fae9
Dan Olszewski <dolszewski@carroll.edu>
2018-02-21 19:44:34 -0700

    Added Global.java so I can configure Spring context and use the Play callback for fetchig dynamic classes

---
+++ app/Global.java
@@ -0,0 +1,20 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+ 
+import play.Application;
+import play.GlobalSettings;
+ 
+public class Global extends GlobalSettings {
+    private ApplicationContext ctx;
+ 
+    @Override
+    public void onStart(Application app) {
+        super.onStart(app);
+        ctx = new AnnotationConfigApplicationContext();
+    }
+ 
+    @Override
+    public <A> A getControllerInstance(Class<A> clazz) {
+        return ctx.getBean(clazz);
+    }
+}
